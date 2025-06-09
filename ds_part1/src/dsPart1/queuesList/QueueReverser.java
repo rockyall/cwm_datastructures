acc60884bcdfaf2fa1f3e55077d4c1fc912f09a0
ricardoochoa <ricardo.ochoa@advancetms.com>
2025-06-08 23:05:39 -0600

    Excercise instructions

--- ds_part1/src/dsPart1/queuesList/QueueReverser.java
+++ ds_part1/src/dsPart1/queuesList/QueueReverser.java
@@ -4,9 +4,12 @@ import dsPart1.stackList.Stack;
 
 public class QueueReverser{
     public int[] reverse(int[] list, int knumber){
+        //1- Given an integer K and a queue of integers, write code to reverse the
+        //order of the first K elements of the queue.
         //Input: Q = [10, 20, 30, 40, 50], K = 3
-        //Output: Q = [30*, 20*, 10*, 40, 50]
+        //Output: Q = [30, 20, 10, 40, 50]
         //Stack: Q = [10, 20, 30, 0, 0] -> [30, 20, 10, 40, 50]
+        //Solution: QueueReverser.reverse()
         //solution: use stacks;
 
         if(knumber > list.length){
